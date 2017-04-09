f3f8f40aae830104d8e33883b55650e44dac9ea8
ocean_deng <ocean_deng@163.com>
2017-04-09 17:00:24 +0800

    update

--- web/scripts/main.js
+++ web/scripts/main.js
@@ -1,7 +1,12 @@
 $(function(){
+    // top-menu 顶部导航调用
+    $('#topMenu').topMenu()
+
+    // main-slide 顶部幻灯插件调用
+    $('#mainSlider').mainSlider()
+
     // card-area 调用
     $('#cardArea').cardArea()
 
 
-    
 })
--- web/scripts/plugin.js
+++ web/scripts/plugin.js
@@ -20,6 +20,76 @@
         })
     }
 
-    
+    /**
+     * topmenu 顶部菜单 底部active条及下拉菜单
+     * @param {string} itemClass 一级菜单class名
+     * @param {string} contentClass 二级菜单class名
+     */
+    var topMenuDefault ={
+        itemClass: '.menu-item',
+        contentClass: '.menu-detail'
+    }
+    $.fn.topMenu = function(options){
+        var options = $.extend({}, topMenuDefault, options),
+            $this = this,
+            mB = '<div class="top-border"></div>',
+            $item = $this.find(options.itemClass);
+
+        return this.each(function(){
+
+            $this.on('mouseenter', function(e){
+                var $_item = $(e.target).parent(),
+                    _w = $_item.width(),
+                    _l = $_item.position().left;
+
+                $this.append(mB)
+                $this.find('.top-border').css({'left': _l}).stop().animate({
+                    width: _w
+                })
+
+            }).on('mouseleave', function(){
+                var $tB = $this.find('.top-border')
+
+                $tB.stop().animate({
+                    width: 0
+                }, function(){
+                    $tB.remove()
+                })
+            })
+
+            $item.on('mouseenter', function(e){
+                var $_this = $(this),
+                    $tB = $this.find('.top-border'),
+                    _w = $_this.width(),
+                    _l = $_this.position().left;
+
+                $tB.stop().animate({'width': _w,'left': _l})
+                $_this.find(options.contentClass).stop().slideDown()
+            }).on('mouseleave', function(e){
+                var $_this = $(this);
+
+                $_this.find(options.contentClass).stop().slideUp()
+            })
+        })
+    }
+
+    /**
+     * top-slide 顶部幻灯
+     *
+     */
+    var mainSliderDefault = {
+
+    }
+    $.fn.mainSlider = function(options){
+        var options = $.extend({}, mainSliderDefault, options)
+
+        return this.each(function(){
+            var $this = $(this)
+
+console.log('a');
+
+        });
+    }
+
 
 })(window, jQuery)
