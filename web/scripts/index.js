327fe40cc7d80f92aeae9585016e0b60f79f2232
ocean_deng <ocean_deng@163.com>
2017-04-11 10:54:16 +0800

    update

---
+++ web/scripts/index.js
@@ -0,0 +1,11 @@
+$(function(){
+    // main-slide 顶部幻灯插件调用
+    $('#mainSlider').mainSlider()
+
+    // card-area 调用
+    $('#cardArea').cardArea()
+
+    // 解决方案切换调用
+    $('#solutionSlide').solutionSlide()
+
+})
--- web/scripts/main.js
+++ web/scripts/main.js
@@ -2,16 +2,6 @@ $(function(){
     // top-menu 顶部导航调用
     $('#topMenu').topMenu()
 
-    // main-slide 顶部幻灯插件调用
-    $('#mainSlider').mainSlider()
-
-    // card-area 调用
-    $('#cardArea').cardArea()
-
-    // 解决方案切换调用
-    $('#solutionSlide').solutionSlide()
-
     // footer二维码导航调用
     $('#attentionMenu').footerMenu()
-
 })
