793c02ca5532cd165ac06525ca537ccf23cacc33
denghaiyang <denghaiyang@300.cn>
2017-05-15 17:23:54 +0800

    up

--- special/scripts/main.js
+++ special/scripts/main.js
@@ -5,7 +5,7 @@
 	        $item = $ele.find('.tool-item'),
 	        body = '.fold-body',
 	        close = '.fold-close',
-	        offset = 5;
+	        offset = 6;
 
 	    return this.each(function(){
 	    	$item.on('mouseenter', function(){
@@ -13,14 +13,14 @@
 	    			$body = $_this.find(body),
 	    			_l = $body.outerWidth();
 
-	    		$body.animate({
-	    			left: - _l + 5
+	    		$body.stop().animate({
+	    			left: - _l + offset
 	    		})
 	    	}).on('mouseleave', function(){
 	    		var $_this = $(this),
 	    			$body = $_this.find(body);
 
-	    		$body.animate({
+	    		$body.stop().animate({
 	    			left: 0
 	    		})
 	    	})
@@ -31,6 +31,7 @@
 
 	$.fn.goDetail = function(){
 		var $ele = this,
+			speed = 200,
 			detail = '.go-detail';
 
 		return this.each(function(){
@@ -39,18 +40,36 @@
 
 				$_this.find(detail).stop().animate({
 					top: 0
-				})
+				}, speed)
 			}).on('mouseleave', function(){
 				var $_this = $(this),
 					_t = $_this.outerHeight();
 
 				$_this.find(detail).stop().animate({
 					top: _t
-				})
+				}, speed)
 			})
 		})
 	}
 
 	$('#toolBar').toolBar()
 	$('.case-img').goDetail()
+
+
+	var $p = $('#getPhone')
+
+	$('#pClose').on('click', function(){
+		$p.animate({
+			left: $p.outerWidth()
+		}, function(){
+			$('.arrow').show()
+		});
+	})
+
+	$('.arrow').on('click', function(){
+		$('.arrow').hide()
+		$p.animate({
+			left: 0
+		})
+	})
 })(window, jQuery)
