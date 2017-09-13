1da879dca723ac74b70b76e947de3c8c9e306903
ocean_deng <ocean_deng@163.com>
2017-09-13 14:00:17 +0800

    up

--- anniversary/scripts/common.js
+++ anniversary/scripts/common.js
@@ -19,3 +19,39 @@ $(function(){
         location.href = $_this.attr('data-href');
     });
 });
+
+
+(function(){
+
+	$(window).load(function(){
+
+		_fullImgScreen('*[data-full-screen]')
+
+		setTimeout(function(){
+			$('.index-img-1').show().addClass('animated bounceInDown')
+		}, 200)
+		setTimeout(function(){
+			$('.top-left-img').show().addClass('animated fadeInLeft')
+			$('.bottom-right-img').show().addClass('animated fadeInRight')
+		},500)
+	})
+	$(window).resize(function(){
+		_fullImgScreen('*[data-full-screen]')
+	})
+
+	function _fullImgScreen(ele){
+		$(ele).each(function(i, v){
+			var $_this = $(v),
+			$_img = $_this.find('img'),
+			_imgw = $_img.width(),
+			_imgh = $_img.height(),
+			ratio = _imgw / _imgh;
+
+			$_img.width(sW)
+			if($_img.height() < sH){
+				$_img.height(sH)
+				$_img.width(parseInt(sH * ratio))
+			}
+		})
+	}
+})()
